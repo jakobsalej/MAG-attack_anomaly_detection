28fd5cda484a3d6dcfe7d99f6ac5adea3c7f61ce
jakob <jakob.salej@gmail.com>
2020-04-02 03:36:57 +0200

    Code refactor & saving results to files

--- data_analysis.py
+++ data_analysis.py
@@ -1,44 +1,146 @@
 import numpy as np
 import pandas as pd
 
-from sklearn.preprocessing import LabelEncoder, scale, StandardScaler
+from sklearn.model_selection import train_test_split, cross_val_score, cross_validate
+from sklearn.metrics import accuracy_score, balanced_accuracy_score, f1_score, precision_score, recall_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import scale, StandardScaler
+
+# from keras.wrappers.scikit_learn import KerasClassifier
+# from ann import ANN
 
 
 class DataAnalysis:
-    def __init__(self, path):
-        self.data = pd.read_csv(path)
+    def __init__(self, data, testSize=0.2, verbose=0):
+        self.data = data
+        self.predictions = None
+        self.verbose = verbose
 
-    def print(self):
-        print(self.data.describe())
-        print(self.data.columns)
-        print(self.data.shape)
+        # separate X and y
+        X = data.drop('normality', axis=1)
+        y = data['normality']
 
-        # output all columns and their unique values
-        for column in self.data.columns:
-            print(column, self.data[column].unique())
+        # scale the X as part of preprocessing
+        xScaled = StandardScaler().fit_transform(X)
+        X = pd.DataFrame(xScaled, columns=X.columns)
 
-    def prepareData(self):
-        # replace 'nan' values in column 'accessedNodeType' with ‘Malicious’
-        self.data['accessedNodeType'] = self.data['accessedNodeType'].fillna(
-            'Malicious')
+        # split data into training and testing set
+        self.xTrain, self.xTest, self.yTrain, self.yTest = train_test_split(
+            X, y, test_size=testSize, random_state=1)
 
-        # replace all non numeric values in column 'value'
-        self.data = self.data.replace(
-            to_replace={'value': {'False': '0.0', 'false': '0.0', 'True': '1.0', 'true': '1.0', 'Twenty': '20.0', 'twenty': '20.0', 'none': '0.0', 'None': '0.0'}})
-        self.data['value'] = pd.to_numeric(self.data['value'], errors='coerce')
+        print('Number of samples:', self.xTrain.shape[0])
 
-        # set missing values to 0
-        self.data['value'].fillna(0, inplace=True)
+    def print(self, data):
+        print('desc', data.describe())
+        print('cols', data.columns)
 
-        # remove 'timestamp' column
-        self.data = self.data.drop(columns=['timestamp'])
+    def getDataCharacteristics(self):
+        # get all unique values (classes) and their count
+        trainClassesCount = self.yTrain.value_counts(sort=False)
+        testClassesCount = self.yTest.value_counts(sort=False)
 
-        # apply label encoding
-        # use label enconding on all columns except column 'value'
-        cols = [col for col in self.data.columns if col not in ['value']]
-        self.data[cols] = self.data[cols].apply(LabelEncoder().fit_transform)
+        # save count for every class
+        allClasses = [0, 1, 2, 3, 4, 5, 6, 7]
+        classesCount = []
+        countSum = 0
+        for c in allClasses:
+            # train classes
+            trainCount = 0
+            if c in trainClassesCount:
+                trainCount = trainClassesCount[c]
 
-    def returnData(self, percentage=1):
-        sampleData = self.data.sample(frac=percentage, random_state=1)
+            # test classes
+            testCount = 0
+            if c in testClassesCount:
+                testCount = testClassesCount[c]
 
-        return sampleData
+            # add both counts to overall sum
+            countSum += (trainCount + testCount)
+
+            # append tuple (train count, test count) of a class to array of results
+            classesCount.append((trainCount, testCount))
+
+        # append overall sum
+        classesCount.append(countSum)
+
+        return classesCount
+
+    def logisticRegression(self):
+        return LogisticRegression(
+            verbose=self.verbose, max_iter=1000, n_jobs=-1)
+
+    def SVM(self):
+        return SVC(verbose=self.verbose)
+
+    def DecisionTree(self):
+        return DecisionTreeClassifier()
+
+    def RandomForest(self, nEstimators=100):
+        return RandomForestClassifier(n_estimators=nEstimators, n_jobs=-1)
+
+    # def ANN(self, epochs=10):
+    #     ann = ANN()
+    #     estimator = KerasClassifier(
+    #         build_fn=ann.getModel, epochs=epochs, verbose=1)
+    #     return estimator
+
+    def predict(self, model, algName):
+        print('\n', algName, ':\n')
+
+        # k=5 cross validation on training set
+        trainScores = cross_validate(
+            model, self.xTrain, self.yTrain, scoring=('accuracy', 'balanced_accuracy', 'f1_weighted', 'precision_weighted', 'recall_weighted'), return_train_score=True)
+
+        trainScores = [trainScores['test_accuracy'].mean(), trainScores['test_accuracy'].std(), trainScores['test_balanced_accuracy'].mean(),
+                       trainScores['test_f1_weighted'].mean(), trainScores['test_precision_weighted'].mean(), trainScores['test_recall_weighted'].mean()]
+
+        print('Training set:', trainScores)
+
+        # fit the model
+        model.fit(self.xTrain, self.yTrain)
+
+        # predict
+        yPredicted = model.predict(self.xTest)
+
+        # get testing set scores
+        acc = accuracy_score(self.yTest, yPredicted)
+        balancedAcc = balanced_accuracy_score(self.yTest, yPredicted)
+        f1 = f1_score(self.yTest, yPredicted, average='weighted')
+        precision = precision_score(self.yTest, yPredicted, average='weighted')
+        recall = recall_score(self.yTest, yPredicted, average='weighted')
+
+        testScores = [acc, balancedAcc, f1, precision, recall]
+
+        print('Testing set:', testScores)
+        print('Unique predicted values:', np.unique(yPredicted))
+
+        return trainScores, testScores
+
+    def getScores(self, selectedAlgorithms=['logReg', 'svm', 'dt', 'rf', 'ann']):
+        logReg = self.logisticRegression()
+        svm = self.SVM()
+        dt = self.DecisionTree()
+        rf = self.RandomForest()
+        # ann = self.ANN(epochs=5)
+
+        # available algorithms ([name, implementation])
+        algorithms = {
+            'logReg': ['Logistic Regression', logReg],
+            'svm': ['SVM', svm],
+            'dt': ['Decision Tree', dt],
+            'rf': ['Random Forest', rf],
+            # 'ann': ['ANN', ann],
+        }
+
+        predictions = {}
+
+        for selected in selectedAlgorithms:
+            [algName, alg] = algorithms[selected]
+            trainScore, testScore = self.predict(alg, algName)
+            predictions[algName] = [trainScore[0], testScore[0]]
+            print(algName, 'train/test accuracy:', trainScore[0], testScore[0])
+
+        return self.xTrain.shape[0], predictions, trainScore, testScore
--- main.py
+++ main.py
@@ -1,35 +1,99 @@
-from data_prediction import DataPrediction
 from data_analysis import DataAnalysis
+from data_preparation import DataPreparation
+
+from datetime import datetime
+import os
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
-# sns.set()
-# np.random.seed(10)
+
+
+def plotResults(train, test, draw=False):
+    # plot results
+    sns.set()
+
+    trainResults = train.melt(
+        'Samples', var_name='Algorithm', value_name='Accuracy')
+    testResults = test.melt(
+        'Samples', var_name='Algorithm', value_name='Accuracy')
+
+    # training accuracy
+    plt.figure(1)
+    trainingPlot = sns.pointplot(x='Samples', y='Accuracy', hue='Algorithm',
+                                 data=trainResults, legend=True, legend_out=True).set_title('Training set (avg. accuracy on 5-fold CV)')
+
+    # testing accuracy
+    plt.figure(2)
+    testingPlot = sns.pointplot(x='Samples', y='Accuracy', hue='Algorithm',
+                                data=testResults, legend=True, legend_out=True).set_title('Testing set')
+
+    # save plot images
+    trainingPlot.get_figure().savefig(dirName + '/training.png')
+    testingPlot.get_figure().savefig(dirName + '/testing.png')
+
+    # draw
+    if draw:
+        plt.show()
+
+
+def createDir(name=datetime.now().strftime("%d-%m-%Y (%H:%M:%S)")):
+    resFolderName = 'results'
+    try:
+        # check if results folder exists already - if not, create it
+        if not os.path.exists(resFolderName):
+            os.mkdir(resFolderName)
+
+        # create new folder for results from this run
+        os.mkdir(resFolderName + '/' + name)
+        return resFolderName + '/' + name
+    except FileExistsError:
+        print("Directory ", name,  " already exists")
+        return None
 
 
 if __name__ == '__main__':
+    # select dataset sizes and algorithms (all options: 'logReg', 'svm', 'dt', 'rf', 'ann')
+    # set both to None to use default values
+    selectedSizes = [0.2, 0.4]
+    selectedAlgorithms = ['dt', 'rf']
+
+    # create new directory for results of this run
+    # name of the folder can be passed as param (default name is timestamp)
+    dirName = createDir()
+
     # analyze and preprocess data
-    da = DataAnalysis('data/mainSimulationAccessTraces.csv')
-    da.prepareData()
+    dp = DataPreparation('data/mainSimulationAccessTraces.csv')
+    dp.prepareData()
+
+    # init dicts to hold data
+    dataInfo = {}
+    fullTrain = {}
+    fullTest = {}
+    trainScores = {'Samples': []}    # scores for plotting
+    testScores = {'Samples': []}     # scores for plotting
 
     # Run predictions
-    trainScores = {'Samples': []}
-    testScores = {'Samples': []}
+    for datasetSize in (selectedSizes or [0.2, 0.4, 0.6, 0.8, 1]):
+        sampleData = dp.returnData(datasetSize)
+        da = DataAnalysis(sampleData)
 
-    for datasetSize in [0.2, 0.4, 0.6, 0.8, 1]:
-        sampleData = da.returnData(datasetSize)
-        dp = DataPrediction(sampleData)
-        noOfSamples, predictions = dp.getScores()
+        # get data characteristics for current dataset size
+        dataInfo[datasetSize] = da.getDataCharacteristics()
 
-        print(noOfSamples, predictions)
+        noOfSamples, predictions, fullTrainScores, fullTestScores = da.getScores(
+            selectedAlgorithms) if selectedAlgorithms else da.getScores()
+
+        # add scores
+        fullTrain[datasetSize] = fullTrainScores
+        fullTest[datasetSize] = fullTestScores
 
         # add no. of samples to dict for plotting
         trainScores['Samples'].append(noOfSamples)
         testScores['Samples'].append(noOfSamples)
 
-        # add accuracies for each algorithm
+        # add accuracies for each algorithm for plotting
         for algName in predictions:
             if algName in trainScores:
                 trainScores[algName].append(predictions[algName][0])
@@ -38,21 +102,21 @@ if __name__ == '__main__':
                 trainScores[algName] = [predictions[algName][0]]
                 testScores[algName] = [predictions[algName][1]]
 
-    # plot results
+    # create pandas dataframes
     trainDF = pd.DataFrame(data=trainScores)
     testDF = pd.DataFrame(data=testScores)
+    dataInfoDF = pd.DataFrame(data=dataInfo)
+    fullTrainDF = pd.DataFrame(data=fullTrain)
+    fullTestDF = pd.DataFrame(data=fullTest)
 
-    trainDF = trainDF.melt(
-        'Samples', var_name='Algorithm', value_name='Accuracy')
-    testDF = testDF.melt(
-        'Samples', var_name='Algorithm', value_name='Accuracy')
+    # save to csv
+    dataInfoDF.to_csv(dirName + '/data_info.csv')
+    fullTrainDF.to_csv(dirName + '/train_results.csv')
+    fullTestDF.to_csv(dirName + '/test_results.csv')
 
-    # training accuracy
-    sns.pointplot(x='Samples', y='Accuracy', hue='Algorithm',
-                  data=trainDF, legend=True, legend_out=True).set_title('Training set')
+    # print(trainDF)
+    # print(dataInfoDF)
+    # print(fullTrainDF)
 
-    # testing accuracy
-    # sns.pointplot(x='Samples', y='Accuracy', hue='Algorithm',
-    #               data=testDF, legend=True, legend_out=True).set_title('Testing set')
-
-    plt.show()
+    # save plots (to also draw them, pass draw=True as param)
+    plotResults(trainDF, testDF)
