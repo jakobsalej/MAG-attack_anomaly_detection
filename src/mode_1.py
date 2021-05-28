9a23c86630482348829fa5f2b5e285c660a6a9f1
Jakob Salej <jakob.salej@gmail.com>
2021-05-28 23:07:28 +0200

    Generated results again for C7 random

--- src/mode_1.py
+++ src/mode_1.py
@@ -190,31 +190,31 @@ def main():
     # Run predictions
     for datasetSize in selectedSizes:
         # Custom mapping
-        # TRAIN_SETS = {
-		#     0.001: 'AD_subset_balanced_0.1.csv',
-		#     0.002: 'AD_subset_balanced_0.2.csv',
-		#     0.005: 'AD_subset_balanced_0.5.csv',
-		#     0.01: 'AD_subset_balanced_1.csv',
-		#     0.02: 'AD_subset_balanced_2.csv',
-		#     0.05: 'AD_subset_balanced_5.csv',
-		#     0.1: 'AD_subset_balanced_10.csv',
-		#     0.15: 'AD_subset_balanced_15.csv',
-		#     0.2: 'AD_subset_balanced_20.csv',
-        # }
-        # trainSetPath = f'../data/AD_datoteke/C7_random/{TRAIN_SETS[datasetSize]}'
-
         TRAIN_SETS = {
-		    0.001: 'AD_set_train_reduced_0.001_0.001.csv',
-		    0.002: 'AD_set_train_reduced_0.002_0.001.csv',
-		    0.005: 'AD_set_train_reduced_0.005_0.001.csv',
-		    0.01: 'AD_set_train_reduced_0.01_0.001.csv',
-		    0.02: 'AD_set_train_reduced_0.02_0.001.csv',
-		    0.05: 'AD_set_train_reduced_0.05_0.001.csv',
-		    0.1: 'AD_set_train_reduced_0.1_0.001.csv',
-		    0.15: 'AD_set_train_reduced_0.15_0.001.csv',
-		    0.2: 'AD_set_train_reduced_0.2_0.001.csv',
+		    0.001: 'AD_subset_balanced_0.1.csv',
+		    0.002: 'AD_subset_balanced_0.2.csv',
+		    0.005: 'AD_subset_balanced_0.5.csv',
+		    0.01: 'AD_subset_balanced_1.csv',
+		    0.02: 'AD_subset_balanced_2.csv',
+		    0.05: 'AD_subset_balanced_5.csv',
+		    0.1: 'AD_subset_balanced_10.csv',
+		    0.15: 'AD_subset_balanced_15.csv',
+		    0.2: 'AD_subset_balanced_20.csv',
         }
-        trainSetPath = f'../data/AD_datoteke/Class_cluster/{TRAIN_SETS[datasetSize]}'
+        trainSetPath = f'../data/AD_datoteke/C7_random/{TRAIN_SETS[datasetSize]}'
+
+        # TRAIN_SETS = {
+		#     0.001: 'AD_set_train_reduced_0.001_0.001.csv',
+		#     0.002: 'AD_set_train_reduced_0.002_0.001.csv',
+		#     0.005: 'AD_set_train_reduced_0.005_0.001.csv',
+		#     0.01: 'AD_set_train_reduced_0.01_0.001.csv',
+		#     0.02: 'AD_set_train_reduced_0.02_0.001.csv',
+		#     0.05: 'AD_set_train_reduced_0.05_0.001.csv',
+		#     0.1: 'AD_set_train_reduced_0.1_0.001.csv',
+		#     0.15: 'AD_set_train_reduced_0.15_0.001.csv',
+		#     0.2: 'AD_set_train_reduced_0.2_0.001.csv',
+        # }
+        # trainSetPath = f'../data/AD_datoteke/Class_cluster/{TRAIN_SETS[datasetSize]}'
         
         print('file', trainSetPath)
 
